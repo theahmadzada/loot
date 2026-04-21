70766e04a668cdfa6d67ee364c17a1610089e905
theahmadzada <ahmad.ahmadzada999@gmail.com>
2026-04-21 21:14:29 +0400

    refactor: change FirstOrDefaultAsync to AnyAsync

---
+++ Loot.Infrastructure/Migrations/20260421171059_MadeBoardNotRequired.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Loot.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MadeBoardNotRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Loot.Infrastructure/Migrations/20260421171230_AddColumnIdToTaskItem.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Loot.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnIdToTaskItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
