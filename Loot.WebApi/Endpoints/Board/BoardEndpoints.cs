aed3daf78a4d9915e7710f488566fc26f0794c97
theahmadzada <ahmad.ahmadzada999@gmail.com>
2026-04-22 17:11:27 +0400

    refactor: make endpoints REST like

--- Loot.WebApi/Endpoints/Board/BoardEndpoints.cs
+++ Loot.WebApi/Endpoints/Board/BoardEndpoints.cs
@@ -8,7 +8,9 @@ public static class BoardEndpoints
 {
     public static WebApplication RegisterBoardEndpoints(this WebApplication app)
     {
-        app.MapPost("/board", async (CreateBoardCommand command, ISender mediator, CancellationToken cancellationToken) =>
+        // var group = app.MapGroup("boards");
+        
+        app.MapPost("/boards", async (CreateBoardCommand command, ISender mediator, CancellationToken cancellationToken) =>
         {
             var result = await mediator.Send(command, cancellationToken);
             return result.Match(value => Results.Ok(value), errors => Results.BadRequest(errors));
--- Loot.WebApi/Endpoints/User/UserEndpoints.cs
+++ Loot.WebApi/Endpoints/User/UserEndpoints.cs
@@ -8,16 +8,24 @@ public static class UserEndpoints
 {
     public static WebApplication RegisterUserEndpoints(this WebApplication app)
     {
-        app.MapPost("/user", async (CreateUserCommand command, ISender mediator, CancellationToken cancellationToken) =>
+        var group = app.MapGroup("users");
+        
+        group.MapPost("register", async (CreateUserCommand command, ISender mediator, CancellationToken cancellationToken) =>
         {
             var result = await mediator.Send(command, cancellationToken);
-            return result.Match(user => Results.Ok(user), errors => Results.BadRequest(errors));
+            return result.Match(value => Results.Ok(value), errors => Results.BadRequest(errors));
         });
 
-        app.MapPost("/email", async (ConfirmEmailCommand command, ISender meadiator, CancellationToken cancellationToken) =>
+        group.MapPost("login", async (LogInUserCommand command, ISender mediator, CancellationToken cancellationToken) =>
+        {
+            var result = await mediator.Send(command, cancellationToken);
+            return result.Match(value => Results.Ok(value), errors => Results.BadRequest(errors));
+        });
+        
+        group.MapPost("email", async (ConfirmEmailCommand command, ISender meadiator, CancellationToken cancellationToken) =>
         {
             var result = await meadiator.Send(command, cancellationToken);
-            return result.Match(id => Results.Ok(id), errors => Results.BadRequest(errors));
+            return result.Match(value => Results.Ok(value), errors => Results.BadRequest(errors));
         });
         
         return app;
